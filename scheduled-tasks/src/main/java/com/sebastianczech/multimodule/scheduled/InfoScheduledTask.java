89d5b9a758d92658847bce2bb3e4c533c4838923
Sebastian Czech <sebaczech@gmail.com>
2019-11-05 22:09:36 +0100

    feature: Run task every 1 second

--- scheduled-tasks/src/main/java/com/sebastianczech/multimodule/scheduled/InfoScheduledTask.java
+++ scheduled-tasks/src/main/java/com/sebastianczech/multimodule/scheduled/InfoScheduledTask.java
@@ -1,12 +1,23 @@
 package com.sebastianczech.multimodule.scheduled;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
 public class InfoScheduledTask {
+
+    private static final Logger log = LoggerFactory.getLogger(InfoScheduledTask.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() {
-        System.out.println("Fixed delay task - " + System.currentTimeMillis() / 1000);
+        log.info("SCHEDULED TASK - INFO - {}", dateFormat.format(new Date()));
     }
 
 }
