d5680f4743311b36414302fee88215decf793b7e
Sebastian Czech <sebaczech@gmail.com>
2019-11-06 21:43:02 +0100

    spring: Use scan base package

--- scheduled-tasks/src/main/java/com/sebastianczech/tasks/ScheduledTasksApplication.java
+++ scheduled-tasks/src/main/java/com/sebastianczech/tasks/ScheduledTasksApplication.java
@@ -3,7 +3,7 @@ package com.sebastianczech.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.sebastiaczech.webapp", "com.sebastianczech.tasks" })
 public class ScheduledTasksApplication {
 
     public static void main(String[] args) {
