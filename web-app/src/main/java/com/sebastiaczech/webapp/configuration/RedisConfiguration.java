e303e887dda226b1b0e64e91c9bf9ade432d312a
Sebastian Czech <sebaczech@gmail.com>
2019-11-17 01:01:24 +0100

    pom, conf: Redis dependencies and configuration

---
+++ web-app/src/main/java/com/sebastiaczech/webapp/configuration/RedisConfiguration.java
@@ -0,0 +1,29 @@
+package com.sebastiaczech.webapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfiguration {
+
+    @Bean
+    private JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("SERVER", 6379);
+        config.setPassword("PASSWORD");
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+
+}
